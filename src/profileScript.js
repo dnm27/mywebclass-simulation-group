f4c9d8b09aecbb5357aa1c8d898a07cc9ebe35cd
dmarriello <79212785+dmarriello@users.noreply.github.com>
2023-03-27 01:26:25 -0400

    Added login and profiles

---
+++ src/profileScript.js
@@ -0,0 +1,6 @@
+const editProfileButton = document.querySelector('button');
+
+editProfileButton.addEventListener('click', () => {
+  // Redirect to edit profile page
+  window.location.href = 'edit-profile.html';
+});
