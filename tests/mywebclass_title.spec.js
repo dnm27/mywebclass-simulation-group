e5015e91fd7199585209a9317db40f52f71cef3b
dmarriello <79212785+dmarriello@users.noreply.github.com>
2023-03-27 23:13:41 -0400

    Added playwright tests for all links on every page

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -8,13 +8,48 @@ test('Should have MyWebClass.org title', async ({ page }) => {
   await page.getByRole('button', { name: 'Agree', exact: true }).click();
     // Get all links on the page
   const links = await page.$$eval('a', (links) => links.map((link) => link.href));
-
   // Loop through each link and check if it's valid
   for (const link of links) {
     const response = await page.goto(link, { timeout: 0, waitUntil: 'load' });
 
     if (response.status() >= 400) {
       await expect(false).toBeTruthy();
+    }
+    else {
+    await expect(true).toBeTruthy();
+    }
+  }
+  await page.getByRole('link', { name: 'Recent Articles' }).click();
+  for (const link of links) {
+    const response = await page.goto(link, { timeout: 0, waitUntil: 'load' });
+
+    if (response.status() >= 400) {
+      await expect(false).toBeTruthy();
+    }
+    else {
+    await expect(true).toBeTruthy();
+    }
+  }
+  await page.getByRole('link', { name: 'Our Story' }).click();
+  for (const link of links) {
+    const response = await page.goto(link, { timeout: 0, waitUntil: 'load' });
+
+    if (response.status() >= 400) {
+      await expect(false).toBeTruthy();
+    }
+    else {
+    await expect(true).toBeTruthy();
+    }
+  }
+  await page.getByRole('link', { name: 'Privacy Policy' }).click();
+  for (const link of links) {
+    const response = await page.goto(link, { timeout: 0, waitUntil: 'load' });
+
+    if (response.status() >= 400) {
+      await expect(false).toBeTruthy();
+    }
+    else {
+    await expect(true).toBeTruthy();
     }
   }
 })
